d94993b5830350a3f837254d4309d22a714f74d0
Russell Gordon <rgordon@lcs.on.ca>
2021-10-02 08:40:51 -0400

    Add array and now saving prediction that is made.

--- macOS/main.swift
+++ macOS/main.swift
@@ -7,8 +7,14 @@
 
 import Foundation
 
+// Create empty list to keep track of predictions made
+var history: [Prediction] = []
+
 // Generate a weather prediction
 let prediction = WeatherPredictionGenerator.getPrediction()
+
+// Save the prediction
+history.append(prediction)
 
 // Show the prediction
 print("Current conditions are \(prediction.condition.description.lowercased()) with a temperature of \(String(format: "%.1f", arguments: [prediction.temperature])) °C.")
