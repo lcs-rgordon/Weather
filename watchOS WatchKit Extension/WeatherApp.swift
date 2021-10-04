ee672d74622397e7d7d42610365656c32c77c7af
Russell Gordon <github@russellgordon.ca>
2021-10-04 06:55:10 -0400

    Added watchOS target.

---
+++ watchOS WatchKit Extension/WeatherApp.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherApp.swift
+//  watchOS WatchKit Extension
+//
+//  Created by Russell Gordon on 2021-10-04.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
